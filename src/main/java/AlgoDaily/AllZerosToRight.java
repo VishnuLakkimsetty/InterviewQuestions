23e881acec951fbef643ce3eb9ee5631a627a41b
Vishnu Lakkimsetty <lakshmi.vishnu.lakkimsetty@oracle.com>
2019-12-06 22:24:14 +0530

    Added AlgoDaily sample question solution

--- src/main/java/AlgoDaily/AllZerosToRight.java
+++ src/main/java/AlgoDaily/AllZerosToRight.java
@@ -3,14 +3,14 @@
 
 /*
  * Solution Approach:
+ * ********************
+ * Create index counter for the array to hold the position where non-zero element to be moved
  * 
- * Create a temp[] of size of given array and a index counter for temp array
- * 
- * Now loop around main array, if non-zero, push to temp and increment index of temp array
- * if zero, increment counter and later push remaining number of zeros to temp array and return it
+ * Now loop around main array, if non-zero, push the element to temp index of a and increment temp index
+ * if zero, leave it and later push zeros to remaining length of array and return it
  * 
  * Time Complexity: O(N)
- * Space Complexity: O(N)
+ * Space Complexity: O(1)
  */
 
 package main.java.AlgoDaily;
@@ -18,28 +18,27 @@ package main.java.AlgoDaily;
 public class AllZerosToRight {
 	public static void main(String[] args) {
 		int[] a = { 1, 0, 4, 2, 0, 5, -9 };
-		a = moveZeros(a);
+		moveZeros(a);
 		for (int i = 0; i < a.length; i++) {
 			System.out.println(a[i]);
 		}
 	}
 
 	static int[] moveZeros(int[] a) {
-		int[] temp = new int[a.length];
 		int temp_index = 0;
 
 		for (int i = 0; i < a.length; i++) {
 			if (a[i] != 0) {
-				temp[temp_index] = a[i];
+				a[temp_index] = a[i];
 				temp_index++;
 			}
 		}
 
-		for (int i = temp_index; i < temp.length; i++) {
-			temp[i] = 0;
+		for (int i = temp_index; i < a.length; i++) {
+			a[i] = 0;
 		}
 
-		return temp;
+		return a;
 	}
 
 }
